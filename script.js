6810764a16b4abf728f0700ec7c81f9d1b96b020
Pola <146646425+poladudek@users.noreply.github.com>
2025-11-19 22:41:42 +0100

    Fix null reference issue

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-const observer = new IntersectionObserver(function(entries) {
-    entries.forEach(function(entry) { 
-        if (entry.isIntersecting) { 
-            if (entry.target.classList.contains('picture-style')) { 
-                entry.target.classList.add('show'); 
-                observer.unobserve(entry.target); 
-            }
-            if(entry.target.classList.contains('about-me-text')){
-                entry.target.classList.add('show');
-                observer.unobserve(entry.target);
-            }
-            if(entry.target.classList.contains('project_card')){
-                entry.target.classList.add('show');
-
-            }
-            if(entry.target.classList.contains('timeline-info')){
-                entry.target.classList.add('show');
-                observer.unobserve(entry.target);
-            }
+const observer = new IntersectionObserver((entries, observer)=>{
+    entries.forEach(entry =>{
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
         }
     });
 });
 
-const projects = document.querySelectorAll('.project_card');
-projects.forEach(project => observer.observe(project));
-/*because it is used by 2 HTML files where one
-only has project_card it is put before everything else
-because after one null code won't continue - a 
-very ugly way for code to work.. change it before final version*/
 
-const picture = document.querySelector('.picture-style');
-const about_me_text = document.querySelector('.about-me-text');
-const time_info = document.querySelectorAll('.timeline-info');
-observer.observe(picture);
-observer.observe(about_me_text);
-time_info.forEach(info => observer.observe(info));
+document.querySelectorAll('.project_card').forEach(el => observer.observe(el));
+document.querySelectorAll('.picture-style').forEach(el => observer.observe(el));
+document.querySelectorAll('.about-me-text').forEach(el => observer.observe(el));
+document.querySelectorAll('.timeline-info').forEach(el => observer.observe(el));
